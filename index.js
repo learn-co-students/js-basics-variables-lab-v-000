e427ff26c44a61909967c9a9535e918a2d43fae8
Elias Ventura <eliasventura23@gmail.com>
2017-11-11 21:52:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Code your solution in this file!
 const companyName = `Scuber`
-
-let mostProfitableNeighborhood = `Chelsea`
-
-let companyCeo = `Susan Smith`
+let mostProfitableNeighborhood = 'Chelsea'
+var companyCeo = "Susan Smith"
