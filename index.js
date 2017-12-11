edf1d64c006123bb51b0ce5f0fae369ae6dc665b
github username <github email address>
2017-12-11 12:17:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+// 
+// companyName = "Scuber";
+//
+// mostProfitableNeighborhood = "Chelsea";
+//
+// companyCeo = "Susan Smith";
