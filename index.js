2dfc4cc9490a96fe25bfa1068274ac5a7a54c50a
Emmanuel Owusu <e_owusu@live.com>
2018-05-09 05:59:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+//var companyName = "specialCompany";
+
+var mostProfitableNeighborhood = "Chelsea";
+
+var companyCeo = "Lauren Hart";
+var companyCeo = "Susan Smith";
