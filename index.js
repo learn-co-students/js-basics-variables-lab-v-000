7df17e13ee0fbee45f0c0decf7e7821b9d8e3fd9
mrjak13 <justinkenney13@gmail.com>
2019-01-29 06:06:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+// companyName = 'specialCompany';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
